040bdb3102aa94fd91680cb6c2fdbe9a6a778161
HBosno <hbosno1@etf.unsa.ba>
2023-02-04 15:58:05 +0100

    'RoutesDaoSQLImpl' now extends AbstractDao class

--- src/main/java/ba/unsa/etf/rpr/dao/RoutesDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoutesDaoSQLImpl.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Route;
+
 /**
  * MySQL implementation of DAO
  * @author Hamza Bosno
  */
 
-public class RoutesDaoSQLImpl extends RoutesDao{
+public class RoutesDaoSQLImpl extends AbstractDao<Route> implements RoutesDao {
 }
