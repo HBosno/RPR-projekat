218afaf68cf6d6c914245879de838a33d73d1ba6
HBosno <hbosno1@etf.unsa.ba>
2023-02-04 15:57:30 +0100

    'CardsDaoSQLImpl' now extends AbstractDao class

--- src/main/java/ba/unsa/etf/rpr/dao/CardsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CardsDaoSQLImpl.java
@@ -1,10 +1,15 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Card;
+
+import java.sql.ResultSet;
+import java.util.Map;
+
 /**
  * MySQL implementation of DAO
  * @author Hamza Bosno
  */
 
-public class CardsDaoSQLImpl implements CardsDao{
+public class CardsDaoSQLImpl extends  AbstractDao<Card> implements CardsDao{
 
 }
