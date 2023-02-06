8ecd19827a77c42fef5a71ffb2401d7c3c92b5a2
HBosno <hbosno1@etf.unsa.ba>
2023-02-06 16:35:40 +0100

    'Route' class fully implemented

--- src/main/java/ba/unsa/etf/rpr/domain/Route.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Route.java
@@ -1,4 +1,63 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Route implements Idable {
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Contains bus routes
+ *
+ * @author Hamza Bosno
+ */
+
+public class Route implements Idable, Serializable{
+
+    private int id;
+    private String route;
+    private String frequency;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "id=" + id +
+                ", route=" + route +
+                ", frequency=" + frequency +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Route route = (Route) o;
+        return id == route.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, route, frequency);
+    }
 }
