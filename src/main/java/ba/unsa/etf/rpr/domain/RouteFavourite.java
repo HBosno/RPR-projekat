dc73f96b39ee8b303afdfa316f1765eab02f754c
HBosno <hbosno1@etf.unsa.ba>
2023-02-06 16:35:56 +0100

    'RouteFavourite' class fully implemented

--- src/main/java/ba/unsa/etf/rpr/domain/RouteFavourite.java
+++ src/main/java/ba/unsa/etf/rpr/domain/RouteFavourite.java
@@ -1,4 +1,63 @@
 package ba.unsa.etf.rpr.domain;
 
-public class RouteFavourite implements Idable {
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * List of routes of interest for certain profiles
+ *
+ * @author Hamza Bosno
+ */
+
+public class RouteFavourite implements Idable, Serializable{
+
+    private int id;
+    private Profile profile;
+    private Route route;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    @Override
+    public String toString() {
+        return "RouteFavourite{" +
+                "id=" + id +
+                ", profile=" + profile +
+                ", route=" + route +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RouteFavourite routefavourite = (RouteFavourite) o;
+        return id == routefavourite.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, profile, route);
+    }
 }
