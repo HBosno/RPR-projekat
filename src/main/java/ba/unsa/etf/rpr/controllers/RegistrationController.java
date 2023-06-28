0affd396edc42b0a12b3cf4e4860e3ea58ae988c
HBosno <hbosno1@etf.unsa.ba>
2023-06-28 14:47:32 +0200

    Added regex for password validation

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -11,4 +11,9 @@ public class RegistrationController {
     public TextField surnameField;
     public PasswordField passwordField;
     public Label passwordWarning;
+
+    public boolean validatePassword(String password){
+        String passwordRegex = "^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{5,}$";
+        return password.matches(passwordRegex);
+    }
 }
