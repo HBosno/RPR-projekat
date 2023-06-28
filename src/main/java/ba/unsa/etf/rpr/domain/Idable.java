3b3c4b88a53ffcce271f79132124f3cc9f356012
HBosno <hbosno1@etf.unsa.ba>
2023-06-28 11:58:47 +0200

    Idable interface javadoc update

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.domain;
 
 /**
  * Interface that forces all POJO beans to have ID field. Name is stupid but per standard
+ * @author Hamza Bosno
  */
 
 public interface Idable {
