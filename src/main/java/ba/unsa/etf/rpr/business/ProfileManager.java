98752d56a2e5c11319432c713cfdb8afee4228b7
HBosno <hbosno1@etf.unsa.ba>
2023-06-28 15:29:36 +0200

    validateLogin javadoc added

--- src/main/java/ba/unsa/etf/rpr/business/ProfileManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ProfileManager.java
@@ -12,6 +12,13 @@ import java.util.List;
  */
 public class ProfileManager {
 
+    /**
+     * Validation method used on login screen. Used to check if there is an existing user in database based on email provided.
+     * After that, checks if entered password matches the one in database for found user.
+     * @param email - email user entered on login
+     * @param password - password user entered on login
+     * @return true if user exists in database, false otherwise
+     */
     public boolean validateLogin(String email, String password){
         List<Profile> profiles;
         try{
