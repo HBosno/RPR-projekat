641fac230dd0d382575675864227fd0ca116c60d
HBosno <hbosno1@etf.unsa.ba>
2023-06-28 12:05:12 +0200

    AppException javadoc added

--- src/main/java/ba/unsa/etf/rpr/exceptions/AppException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/AppException.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * User defined exception. Used to catch errors for different SQL queries.
+ * @author Hamza Bosno
+ */
 public class AppException extends Exception{
 
     public AppException(String msg, Exception reason){
