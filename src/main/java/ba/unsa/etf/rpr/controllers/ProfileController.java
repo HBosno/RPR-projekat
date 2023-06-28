06f9c51e7439ce45dc7284e1be54e7a46311ce55
HBosno <hbosno1@etf.unsa.ba>
2023-06-28 20:31:36 +0200

    Profile controller added

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ProfileController {
+    public TextField emailField;
+    public TextField nameField;
+    public TextField surnameField;
+    public TextField adressField;
+    public TextField telephoneField;
+    public Button confirmButton;
+    public Button cancelButton;
+
+    public void confirmButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonOnClick(ActionEvent actionEvent) {
+    }
+}
