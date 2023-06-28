a03a081a8a51080d3d6e2657a22d6bcb6b384496
HBosno <hbosno1@etf.unsa.ba>
2023-06-28 14:28:56 +0200

    validation labels for registration screen

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegistrationController {
+    public TextField emailField;
+    public Label emailWarning;
+    public TextField nameField;
+    public TextField surnameField;
+    public PasswordField passwordField;
+    public Label passwordWarning;
 }
