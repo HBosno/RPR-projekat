5d9171e6e931c446771a98030970279ad0419d35
HBosno <hbosno1@etf.unsa.ba>
2023-06-29 06:29:40 +0200

    email validation in edit profile

--- src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
@@ -53,6 +53,11 @@ public class ProfileController {
     public void confirmButtonOnClick(ActionEvent actionEvent) {
     }
 
+    public boolean validateEmail(String email){
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        return email.matches(emailRegex);
+    }
+
     public void cancelButtonOnClick(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/dashboard.fxml"));
