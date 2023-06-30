09341f1e36092e1a86f5f50ea3c603ed4c93b8fe
HBosno <hbosno1@etf.unsa.ba>
2023-07-01 01:53:13 +0200

    Populating listview with card numbers

--- src/main/java/ba/unsa/etf/rpr/controllers/CardsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CardsController.java
@@ -1,17 +1,24 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.CardManager;
 import ba.unsa.etf.rpr.business.ProfileManager;
+import ba.unsa.etf.rpr.domain.Card;
 import ba.unsa.etf.rpr.domain.Profile;
 import ba.unsa.etf.rpr.exceptions.AppException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardsController {
 
     private final ProfileManager profileManager = new ProfileManager();
-    public ListView cardsList;
+    private final CardManager cardManager = new CardManager();
+    public ListView<String> cardsList;
     public Label serialNumberField;
     public Label cardTypeField;
     public Label couponField;
@@ -24,6 +31,16 @@ public class CardsController {
 
     public CardsController(String email) throws AppException {
        user = profileManager.getProfileByEmail(email);
+    }
+
+    @FXML
+    public void initialize() throws AppException {
+        List<Card> cards = cardManager.getUserCards(user.getId());
+        List<String> serialNumbers = new ArrayList<>();
+        for(Card card: cards){
+            serialNumbers.add(String.valueOf(card.getSerialNumber()));
+        }
+        cardsList.getItems().addAll(serialNumbers);
     }
 
     public void depositButtonOnClick(ActionEvent actionEvent) {
