28dbe99eaa48ce51a2be1bfcef1bfd8e27b58f75
HBosno <hbosno1@etf.unsa.ba>
2023-06-30 11:54:56 +0200

    CardsController added

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/CardsController.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class CardsController {
+    public ListView cardsList;
+    public Label serialNumberField;
+    public Label cardTypeField;
+    public Label couponField;
+    public Label balanceField;
+    public Button depositButton;
+    public Button activateCouponButton;
+    public Button addCardButton;
+    public Button backButton;
+
+    public void depositButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void activateCouponButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void addCardButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void backButtonOnClick(ActionEvent actionEvent) {
+    }
+}
