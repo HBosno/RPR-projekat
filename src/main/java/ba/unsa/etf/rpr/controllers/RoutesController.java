e7f65c6edd4da12e25b68e9c42a697fc719579e5
HBosno <hbosno1@etf.unsa.ba>
2023-07-02 14:32:50 +0200

    successfully populating routes list

--- src/main/java/ba/unsa/etf/rpr/controllers/RoutesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RoutesController.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.RouteManager;
+import ba.unsa.etf.rpr.domain.Route;
+import ba.unsa.etf.rpr.exceptions.AppException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,7 +10,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoutesController {
+    private final RouteManager routeManager = new RouteManager();
     public ListView<String> routesList;
     public Label relationLabel;
     public Label frequencyLabel;
@@ -23,8 +30,13 @@ public class RoutesController {
     }
 
     @FXML
-    public void initialize(){
-
+    public void initialize() throws AppException {
+        List<Route> routes = routeManager.getAllRoutes();
+        List<String> routeNames = new ArrayList<>();
+        for(Route route: routes){
+            routeNames.add(route.getRoute());
+        }
+        routesList.getItems().addAll(routeNames);
     }
 
     public void addButtonOnClick(ActionEvent actionEvent) {
