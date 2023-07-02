67a44c777923cb6c621a07858b88fb0398edd0e5
HBosno <hbosno1@etf.unsa.ba>
2023-07-02 10:05:56 +0200

    addcard controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddCardController.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+
+/**
+ * Controller for managing add card user interface.
+ * @author Hamza Bosno
+ */
+
+public class AddCardController {
+
+    public TextField serialNumberField;
+    public ChoiceBox choiceBox;
+    public Button addButton;
+    public Button cancelButton;
+
+    public void addButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonOnClick(ActionEvent actionEvent) {
+    }
+}
