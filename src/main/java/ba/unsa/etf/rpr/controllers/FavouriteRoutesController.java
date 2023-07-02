ec6386a86c1244eae853fccae8f535298f3b4721
HBosno <hbosno1@etf.unsa.ba>
2023-07-02 17:39:57 +0200

    favouriteroutes controller added

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/FavouriteRoutesController.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+
+/**
+ * Controller for managing favourite routes interface interactions.
+ * @author Hamza Bosno
+ */
+public class FavouriteRoutesController {
+
+    public ListView routesList;
+    public Label relationLabel;
+    public Label frequencyLabel;
+    public RadioButton workDaysRadioButton;
+    public RadioButton weekendRadioButton;
+    public Button backButton;
+
+    public void backButtonOnClick(ActionEvent actionEvent) {
+    }
+}
