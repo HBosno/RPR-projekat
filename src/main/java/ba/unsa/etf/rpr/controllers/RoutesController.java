6d36f1849279c46f994efdffbf7092f88e6c3433
HBosno <hbosno1@etf.unsa.ba>
2023-07-02 14:21:10 +0200

    routesController added

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/RoutesController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+
+public class RoutesController {
+    public ListView routesList;
+    public Label relationLabel;
+    public Label frequencyLabel;
+    public RadioButton workDaysRadioButton;
+    public RadioButton weekendRadioButton;
+    public Button addButton;
+    public Button removeButton;
+    public Button backButton;
+
+    public void addButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void removeButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    public void backButtonOnClick(ActionEvent actionEvent) {
+    }
+}
